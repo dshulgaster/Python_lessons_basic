f9e3d94de107f542792c0d16405d4f3c8676e8c9
dshulgaster <32304020+dshulgaster@users.noreply.github.com>
2019-05-07 15:50:15 +0300

    Easy адаптировано под Normal, доработана ч.1 Normal (Lesson5)

--- lesson05/home_work/hw05_easy.py
+++ lesson05/home_work/hw05_easy.py
@@ -1,12 +1,12 @@
 import os
-# Задача-1:
-# Напишите скрипт, создающий директории dir_1 - dir_9 в папке,
-# из которой запущен данный скрипт.
-# И второй скрипт, удаляющий эти папки.
+import shutil as sh
 
-print("Задача 1. Создать директории dir_1 - dir_9 в текущей папке.\n", '-' * 59, sep='')
+# отдельно выделенные функции для их последующего импорта
+def make_dir_1__9():
+    '''
+    make_dir_1__9: функция создания директорий dir_1 - dir_9
+    '''
 
-def make_dir_1__10():   # функция создания директорий dir_1 - dir_9
     path = os.getcwd()
     for i in range(1, 10):
         path_i = os.path.join(path, 'dir_' + str(i))
@@ -17,7 +17,11 @@ def make_dir_1__10():   # функция создания директорий d
         except FileExistsError:
             print(f"Невозможно создать папку по данному пути: {path_i}")
 
-def del_dir_1__10():    # функция удаления директорий dir_1 - dir_9
+def del_dir_1__9():
+    '''
+    del_dir_1__9: функция удаления директорий dir_1 - dir_9
+    '''
+
     path = os.getcwd()
     for i in range(1, 10):
         path_i = os.path.join(path, 'dir_' + str(i))
@@ -28,41 +32,34 @@ def del_dir_1__10():    # функция удаления директорий d
         except FileNotFoundError:
             print(f"Невозможно удалить папку по данному пути: {path_i}")
 
-
-make_dir_1__10()
-# del_dir_1__10()
-print()
-
-# Задача-2:
-# Напишите скрипт, отображающий папки текущей директории.
-print("Задача 2. Отобразить папки текущей директории.\n", '-' * 46, sep='')
 def show_dir():
+    '''
+    show_dir: функция вывода папок в текущей директории
+    '''
     currentPath = os.getcwd()
-    for i in os.listdir(currentPath):  # вариант 1
-        dir_path = os.path.join(currentPath, i)
-        if os.path.isdir(dir_path):
-            print(i)
-            # print(dir_path)
-
-    print("\nВторой вариант (одной строкой):")
-    print([i for i in os.listdir(currentPath) if os.path.isdir(os.path.join(currentPath, i))]) # вариант 3
-
-    print("\nТретий вариант (os.walk):")
-    for dirs in os.walk(currentPath): # вариант 3 - выводит и текущую папку, подумать, как доработать
-        print(dirs[1])
-        break
-
-    print("\nЧетвертый вариант (os.walk, но одной строкой):")
+    # print("\nЧетвертый вариант (os.walk, но одной строкой):")
     print(next(os.walk(currentPath))[1])
 
-show_dir()
-print()
+    # print("\nПервый вариант:")
+    # for i in os.listdir(currentPath):  # вариант 1
+    #     dir_path = os.path.join(currentPath, i)
+    #     if os.path.isdir(dir_path):
+    #         print(i)
+    #         # print(dir_path)
+    #
+    # print("\nВторой вариант (одной строкой):")
+    # print([i for i in os.listdir(currentPath) if os.path.isdir(os.path.join(currentPath, i))]) # вариант 2
+    #
+    # print("\nТретий вариант (os.walk):")
+    # for dirs in os.walk(currentPath): # вариант 3 - выводит и текущую папку, подумать, как доработать
+    #     print(dirs[1])
+    #     break
 
-
-# Задача-3:
-# Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
-import shutil as sh
 def copy_file():
+    '''
+    copy_file: функция копирования копии текущего файла
+    '''
+
     # nameFile = os.path.realpath(__file__)
     # newFile = nameFile + '_copy'
     newFile = 'hw05_easy_copy.py'
@@ -75,6 +72,39 @@ def copy_file():
     else:
         print(f"Такой файл уже существует")
 
-print("Задача 3. Сделать копию файла, из которого запущен данный скрипт.\n", '-' * 65, sep='')
+# функция для заданий hw05_normal
+def change_dir():
+    '''
+    Запрашиваем у пользователя наименование папки, в которую
+    необходимо перейти и осуществляем переход
+    :return:
+    '''
+    newDir = input('Введите наименование папки для перехода в нее: ')
+    dirPath = os.path.join(os.getcwd(), newDir)
+    if os.path.exists(dirPath):
+        os.chdir(dirPath)
+        print(f"Переход в папку '{newDir}' осуществлен")
+    else:
+        print(f"Переход в папку '{newDir}' невозможен")
 
-copy_file()
+if __name__ == '__main__':
+    # Задача-1:
+    # Напишите скрипт, создающий директории dir_1 - dir_9 в папке,
+    # из которой запущен данный скрипт.
+    # И второй скрипт, удаляющий эти папки.
+
+    print("Задача 1. Создать директории dir_1 - dir_9 в текущей папке.\n", '-' * 59, sep='')
+    make_dir_1__9()
+    # del_dir_1__9()
+    print()
+
+    # Задача-2:
+    # Напишите скрипт, отображающий папки текущей директории.
+    print("Задача 2. Отобразить папки текущей директории.\n", '-' * 46, sep='')
+    show_dir()
+    print()
+
+    # Задача-3:
+    # Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
+    print("Задача 3. Сделать копию файла, из которого запущен данный скрипт.\n", '-' * 65, sep='')
+    copy_file()
